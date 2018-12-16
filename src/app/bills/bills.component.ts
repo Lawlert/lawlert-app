8116f1780c817d6ea125623f0f81c4f26acff17e
Jose Gonzalez <jgpereira23@gmail.com>
2018-12-16 18:54:40 -0500

    Retrieve bills from server and list them

--- src/app/bills/bills.component.ts
+++ src/app/bills/bills.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BillsService } from './bills.service';
-import { Bill } from './bills.model';
+import { Bill } from './bill.model';
+import { Subscription } from 'rxjs';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-bills',
   templateUrl: './bills.component.html',
   styleUrls: ['./bills.component.css']
 })
-export class BillsComponent implements OnInit {
+export class BillsComponent implements OnInit, OnDestroy {
   bills: Bill[] = [];
+  private senatorsSub: Subscription;
 
-  constructor( public billsService: BillsService) {}
+  constructor( public billsService: BillsService, public snackBar: MatSnackBar) {}
 
   ngOnInit() {
+    this.billsService.getSenators();
+    this.senatorsSub = this.billsService.getSenatorsUpdateListener()
+      .subscribe((bills: Bill[]) => {
+        this.bills = bills;
+      });
   }
 
+  ngOnDestroy() {
+    this.senatorsSub.unsubscribe();
+  }
+
+  onFollowClick(bill: Bill) {
+    this.snackBar.open('You\'ve followed Bill #:' + bill.number + '!');
+  }
 }
--- src/app/bills/bills.service.ts
+++ src/app/bills/bills.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+import { Bill } from './bill.model';
+import { Subject } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class BillsService {
+  private bills: Bill[] = [];
+  private billsUpdated = new Subject<Bill[]>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSenators() {
+    this.http.get<{message: string, bills: Bill[]}>('http://localhost:3000/api/bills')
+      .subscribe((billData) => {
+        this.bills =  billData.bills;
+        this.billsUpdated.next([...this.bills]);
+    });
+  }
+
+  getSenatorsUpdateListener() {
+    return this.billsUpdated.asObservable();
+  }
+
 }
